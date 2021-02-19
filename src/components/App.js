e26d0ace1fe620c25fabb5bb23bc3d45d048b90c
shubhammahamuni <shubham.mahamuni.902@gmail.com>
2021-02-19 19:07:45 +0530

    the final submit

--- src/components/App.js
+++ src/components/App.js
@@ -11,23 +11,43 @@ class App extends Component {
         };
         this.renderChoice = this.renderBallOrButton.bind(this)
         this.buttonClickHandler = this.buttonClickHandler.bind(this)
+        this.handlekeypress = this.handlekeypress.bind(this)
     };
 
     buttonClickHandler() {
+        this.setState({renderBall : true});
    
    }
     renderBallOrButton() {
 		if (this.state.renderBall) {
 		    return <div className="ball" style={this.state.ballPosition}></div>
 		} else {
-		    return <button onClick={this.buttonClickHandler} >Click For One Ball</button>
+		    return <button onClick={this.buttonClickHandler}>Click For One Ball</button>
 		}
     }
 
     // bind ArrowRight keydown event
     componentDidMount() {
-      
+        document.addEventListener("keydown" , this.handlekeypress)
     }
+    
+
+    handlekeypress(e){ 
+        
+       if(e.keyCode==39)
+       {
+           let newposition  = this.state.posi + 5;
+           this.setState({
+               posi : newposition,
+               ballPosition : { left: newposition+"px"},
+           });
+           //console.log(newposition);
+
+       }
+       
+
+    }
+   
 
     render() {
         return (
